d9f0f90617123f1d3e53f84e06a5d23c7bb8ffaa
Payton Lower <paytown@me.com>
2019-01-16 20:41:51 -0800

    some new JavaScript stuff

--- main.js
+++ main.js
@@ -1,2 +1,38 @@
-var sum = 3 + 4;
-console.log('hello console!', sum);
+var x = 12;
+
+
+if(x > 12) {
+    console.log(x);
+}
+else {
+    console.log('ya cooked it mate');
+}
+
+
+for(var i = 0; i < x; i++) {
+    // displays only even numbers
+    if(i % 2 === 0){
+        console.log(i);
+    }
+}
+
+function sayHello() {
+    console.log('hi')
+}
+
+sayHello();
+
+
+//eslint-dsable-next-line
+function evenOrOdd() {
+    var input = document.getElementById('number');
+
+    var y = input.value;
+
+    if(y % 2 === 0){
+        console.log('even');
+    }
+    else {
+        console.log('odd');
+    }
+}
